9ff37f173e8d2663ec4f6be7b9c32faf4c69a277
Mane Kirakosyan <kirakosyanmane03@gmail.com>
2020-10-25 01:14:03 +0400

    fix bugs on js files

--- build/js/script.js
+++ build/js/script.js
@@ -1,51 +1,22 @@
-// $('.question__block').click(function () {
-// 	let container = $(this).parents('.question');
-// 	let answer = container.find('.question__answer');
-// 	let trigger = container.find('.question__arrow');
-//
-// 	answer.slideToggle(200);
-//
-// 	if (trigger.hasClass('question__rotated')) {
-// 		trigger.removeClass('question__rotated');
-// 	} else {
-// 		trigger.addClass('question__rotated');
-// 	}
-//
-// 	if (container.hasClass('expanded')) {
-// 		container.removeClass('expanded');
-// 	} else {
-// 		container.addClass('expanded');
-// 	}
-// });
-// jQuery(document).ready(($) => {
-// 	$('.accordion__block').click(function () {
-// 		$(this).find('.accordion__close').toggleClass('open');
-// 		$(this)
-// 			.next('.accordion__open')
-// 			.slideToggle('fast', () => {
-// 				// Do something maybe ..
-// 			});
-// 	});
-//
-// $('.question__block').click(function () {
-// 	let container = $(this).parents('.question');
-// 	let answer = container.find('.question__answer');
-// 	let trigger = container.find('.question__arrow');
-//
-// 	answer.slideToggle(200);
-//
-// 	if (trigger.hasClass('question__rotated')) {
-// 		trigger.removeClass('question__rotated');
-// 	} else {
-// 		trigger.addClass('question__rotated');
-// 	}
-//
-// 	if (container.hasClass('expanded')) {
-// 		container.removeClass('expanded');
-// 	} else {
-// 		container.addClass('expanded');
-// 	}
-// });
+$('.question__block').click(function () {
+	let container = $(this).parents('.question');
+	let answer = container.find('.question__answer');
+	let trigger = container.find('.question__arrow');
+
+	answer.slideToggle(200);
+
+	if (trigger.hasClass('question__rotated')) {
+		trigger.removeClass('question__rotated');
+	} else {
+		trigger.addClass('question__rotated');
+	}
+
+	if (container.hasClass('expanded')) {
+		container.removeClass('expanded');
+	} else {
+		container.addClass('expanded');
+	}
+});
 
 
 
@@ -61,3 +32,15 @@ $(".menu").click(function (e) {
 		}
 	})
 })
+
+
+
+jQuery(document).ready(($) => {
+	$('.accordion__block').click(function () {
+		$(this).find('.accordion__close').toggleClass('open');
+		$(this)
+			.next('.accordion__open')
+			.slideToggle('fast', () => {
+				// Do something maybe ..
+			});
+	})})
